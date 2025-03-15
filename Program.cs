127560d9d0e926cf193cf840b96dee375e4845e3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:33:56 -0300

    Atividade Finalizada

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Eu te darei o ceu");
+Console.ResetColor();
+
+Console.Write("---\n---\n---\n"); 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+Eu te darei o céu meu bem!
+E o meu amor também
+Eu te darei o céu meu bem!
+E o meu amor também");
+
+
+Console.Write("---\n---\n---\n");
+Console.ResetColor();
+Console.WriteLine(@"
+Quanto tempo eu vivi a procurar
+Por você, meu bem até lhe encontrar
+Mas se você pensar em me deixar
+Farei o impossível prá ficar até!");
+
+
+Console.Write("---\n---\n---\n");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+Eu te darei o céu meu bem!
+E o meu amor também
+Eu te darei o céu meu bem!
+E o meu amor também");
+
+
+Console.ResetColor();
+Console.WriteLine(@"
+Você pode até gostar de outro rapaz
+Que lhe dê amor carinho e muito mais
+Porém mais do que eu ninguém vai dar
+Até o infinito eu vou buscar e então!");
+
+
+Console.Write("---\n---\n---\n");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+Eu te darei o céu meu bem!
+E o meu amor também
+Eu te darei o céu meu bem!
+E o meu amor também");
+
+
+Console.Write("---\n---\n---\n");
+Console.ResetColor();
+Console.WriteLine(@"
+Toda a minha vida eu já te dei
+E agora já não sei
+O que vou fazer se te perder eu morrerei!");
+
+
+Console.Write("---\n---\n---\n");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+Ah! Eu te darei o céu meu bem!
+E o meu amor também
+Eu te darei o céu meu bem!
+E o meu amor também");
+
+
